bb4f88d71cc335495bf1623ba7027b16d054f8d0
varo <varo@pop-os.localdomain>
2020-06-10 17:26:47 +0200

    br en fichar

--- inc/contenido-home.php
+++ inc/contenido-home.php
@@ -1,7 +1,7 @@
 <div class="container" style="margin-top:50px">
   <h3>Contenido de la Página de inicio</h3>
   <p>Podemos morstrar una tabla que muestre las clases de hoy del profesor que haya iniciado sesión.</p>
-  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar'; ?>"><span  class="glyphicon glyphicon-ok"></span> Fichar Entrada</a>
+  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar'; ?>"><span  class="glyphicon glyphicon-ok"></span> Fichar Entrada</a><br>
   <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar_salida'; ?>"><span  class="glyphicon glyphicon-ok"></span> Fichar Salida</a>
 </div>
 <?php
