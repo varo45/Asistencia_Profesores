8273da3063af6579843149d92762695209461236
varo <varo@pop-os.localdomain>
2020-06-12 13:11:38 +0200

    contenidohome

--- inc/contenido-home.php
+++ inc/contenido-home.php
@@ -6,40 +6,23 @@
 </div>
 <?php
 
-
-function tooLateEntrada()
-{
-    $bd->bdConex();
-    $conex = $bd->conex;
-    $id = $bd->getID();
-    if($bd->conex_status == 1)
-    {
-        date_default_timezone_set('Europe/Madrid');
-        $fecha = date('Y-m-d');
-        $horaactual = mktime(date('H'),date('i'),date('s'),date('m'),date('d'),date('Y'));
-        $hora_salida = $bd->getHoraSalida($bd->getDiaSemana());
-        return $hora_salida;
-
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+  date_default_timezone_set('Europe/Madrid');
+  $ahora = getdate();
+  var_dump($ahora);
 
 
 
-if(isset($ERR_BD))
+
+if(isset($ERR_MSG))
 {
   echo <<< EOL
   <script>
   window.onload = function() {
-    $('#ERR_BD_MODAL').modal('show')
+    $('#ERR_MSG_MODAL').modal('show')
   };
   </script>
   <!-- Modal -->
-  <div class="modal fade" id="ERR_BD_MODAL" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal fade" id="ERR_MSG_MODAL" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -49,34 +32,7 @@ if(isset($ERR_BD))
         </div>
         <div class="modal-body">
           <p style="color: red;">
-            $ERR_BD
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-EOL;
-}
-elseif(isset($MSG_BD))
-{
-  echo <<< EOL
-  <script>
-  window.onload = function() {
-    $('#MSG_BD_MODAL').modal('show')
-  };
-  </script>
-  <!-- Modal -->
-  <div class="modal fade" id="MSG_BD_MODAL" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p style="color: red;">
-            $MSG_BD
+            $ERR_MSG
           </p>
         </div>
       </div>
