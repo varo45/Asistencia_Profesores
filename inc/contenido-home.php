1d6909c1bebeae7cbe8d3c49a09fd67c91f2e70f
Carlos <carlos@pop-os.localdomain>
2020-06-24 12:18:07 +0200

    elimino botón registrar fichaje

--- inc/contenido-home.php
+++ inc/contenido-home.php
@@ -1,6 +1,6 @@
 <div class="container" style="margin-top:50px">
-  <h3>Fichaje</h3>
-  <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar'; ?>"><button type="button" class="btn btn-success"><span  class="glyphicon glyphicon-log-in"></span> Fichar Asistencia</button></a><br><br>
+  <!--h3>Fichaje</h3-->
+  <!--a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar'; ?>"><button type="button" class="btn btn-success"><span  class="glyphicon glyphicon-log-in"></span> Fichar Asistencia</button></a><br><br-->
   <!--a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=fichar_salida'; ?>"><button type="button" class="btn btn-danger"><span  class="glyphicon glyphicon-log-out"></span> Fichar Salida</button></a><br><br><hr-->  
 
 </div>
