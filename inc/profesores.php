0d2db4c14e0b6bc5ac3876af151bdf606b2ff1c4
Carlos <carlos@pop-os.localdomain>
2020-06-26 10:45:05 +0200

    payaso

--- inc/profesores.php
+++ inc/profesores.php
@@ -1,7 +1,7 @@
 <?php
 if($_SESSION['Perfil'] === 'Admin')
 { 
- if ($response = $class->selectFrom("SELECT $class->profesores.ID, $class->profesores.Nombre, $class->profesores.DNI, $class->perfiles.Tipo FROM $class->profesores INNER JOIN $class->perfiles ON $class->profesores.TIPO=$class->perfiles.ID"))
+ if ($response = $class->selectFrom("SELECT $class->profesores.ID, $class->profesores.Nombre, $class->profesores.Iniciales, $class->perfiles.Tipo FROM $class->profesores INNER JOIN $class->perfiles ON $class->profesores.TIPO=$class->perfiles.ID"))
  {
    if ($response->num_rows > 0)
    {
@@ -22,13 +22,11 @@ if($_SESSION['Perfil'] === 'Admin')
             echo "<tr id='profesor_$fila[ID]' class='row_show'>";
             echo "<td>$fila[ID]</td>";
             echo "<td>$fila[Nombre]</td>";
-            echo "<td><span class='glyphicon glyphicon-eye-close muestra'></span> <span class='muestra' style='background-color: black; color: black; border-radius: 4px; padding: 4px;'>XXXXXXXXX</span> 
-            <span class='glyphicon glyphicon-eye-open oculta'></span> <span class='oculta' style='border-radius: 4px; padding: 4px; margin-left: 4px;'>$fila[Iniciales]</span></td>";
+            echo "<td>$fila[Iniciales]</td>";
             echo "<td>$fila[Tipo]</td>";
         }
     echo "</tbody>";
     echo "</table>";
-    include_once($dirs['public'] . 'js/show_dni.js');
     include_once($dirs['public'] . 'js/profesores.js');
     include_once($dirs['inc'] . 'registrar-profesor.php');
    }
--- inc/registrar-profesor.php
+++ inc/registrar-profesor.php
@@ -1,29 +1,38 @@
 <?php
 if(isset($_POST['boton']))
 {
-    if(! $class->validFormName($_POST['nombre']))
+    if(! $class->validFormName($_POST['Nombre']))
     {
-        $ERR_MSG = "Formato de nombre incorrecto.";
+        $ERR_MSG = "Formato de Nombre incorrecto.";
     }
-    elseif(! $class->validFormName($_POST['Iniciales']))
+    elseif(! $class->validFormIni($_POST['Iniciales']))
     {
         $ERR_MSG = "Formato de iniciales incorrecto.";
     }
     else
     {
-        if($class->insertInto("INSERT INTO $class->profesores VALUES ('$_POST[nombre]', '$_POST[Iniciales]')"))
+        if($class->searchDuplicateField($_POST['Iniciales'], 'Iniciales', $class->profesores))
         {
-            $MSG = "Profesor añadido correctamente.";
+            if($class->insertInto("INSERT INTO $class->profesores (Nombre, Iniciales, Password, TIPO, Instituto) 
+            VALUES ('$_POST[Nombre]', '$_POST[Iniciales]', 'f36c0d6388963313095f349dabd4c2e9f730868e', '2', 'IES Bezmiliana')"))
+            {
+                $MSG = "Profesor añadido correctamente.";
+            }
+            else
+            {
+                $ERR_MSG = $class->ERR_NETASYS;
+            }
         }
         else
         {
-            $ERR_MSG = $class->ERR_NETASYS;
+            $ERR_MSG = "No se pueden duplicar las iniciales.";
         }
+
     }
     echo "<div>";
         echo "<form action='$_SERVER[REQUEST_URI]' method='POST'>";
-        echo "<input type='text' name='nombre' value='$_POST[nombre]' placeholder='Nombre y Apellidos'><br><br>";
-        echo "<input type='text' name='iniciales' value='$_POST[iniciales]' placeholder='Iniciales'><br><br>";
+        echo "<input type='text' name='Nombre' value='$_POST[Nombre]' placeholder='Nombre y Apellidos'><br><br>";
+        echo "<input type='text' name='Iniciales' value='$_POST[Iniciales]' placeholder='Iniciales'><br><br>";
         echo "<button value='registrar_prof' name='boton' class='btn btn-default' onclick='return confirm(\"¿Desea registrar a este profesor?\")'>Registrar</button>";
         echo "</form>";
     echo "</div>";
@@ -32,8 +41,8 @@ else
 {
 echo "<div>";
     echo "<form action='$_SERVER[REQUEST_URI]' method='POST'>";
-    echo "<input type='text' name='nombre' value='$_POST[nombre]' class='btn btn-default' placeholder='Nombre y Apellidos'><br><br>";
-    echo "<input type='text' name='iniciales' value='$_POST[iniciales]' class='btn btn-default' placeholder='Iniciales'><br><br>";
+    echo "<input type='text' name='Nombre' value='$_POST[Nombre]' class='btn btn-default' placeholder='Nombre y Apellidos'><br><br>";
+    echo "<input type='text' name='Iniciales' value='$_POST[Iniciales]' class='btn btn-default' placeholder='Iniciales'><br><br>";
     echo "<button value='registrar_prof' name='boton' class='btn btn-default' onclick='return confirm(\"¿Desea registrar a este profesor?\")'>Registrar</button>";
     echo "</form>";
 echo "</div>";
