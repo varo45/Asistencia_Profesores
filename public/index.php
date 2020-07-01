cf6d009151658b1bd6ea6c3a5ec8a0f7b9beb52c
varo <varo@pop-os.localdomain>
2020-07-01 12:01:39 +0200

    index.php

--- public/index.php
+++ public/index.php
@@ -168,7 +168,31 @@ if(isset($_GET['ACTION']))
   {
     if($class->isLogged())
     {
+      $extras = <<< EOL
+      <style>
+          canvas {box-shadow: 4px 4px 8px black; padding: 2px; }
+          .respuesta span {display: block;box-shadow: 4px 4px 8px black; padding: 50px; border-radius: 10px; margin-top: 30px;}
+      </style>
+      <script type="text/javascript" src="js/jsqrcode/grid.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/version.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/detector.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/formatinf.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/errorlevel.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/bitmat.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/datablock.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/bmparser.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/datamask.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/rsdecoder.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/gf256poly.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/gf256.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/decoder.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/qrcode.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/findpat.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/alignpat.js"></script>
+      <script type="text/javascript" src="js/jsqrcode/databr.js"></script>
+      EOL;
       $act_home = 'active';
+
       include($dirs['inc'] . 'top-nav.php');
       include($dirs['inc'] . 'contenido-home.php');
       include($dirs['inc'] . 'qr-reader.php');
