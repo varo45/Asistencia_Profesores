ca3d875494e8f3c80e623ec598a35c1beb68a5da
Carlos <carlos@pop-os.localdomain>
2020-07-02 11:22:08 +0200

    añadido boton "todos los edificios"

--- inc/filtro-edif-guardias.php
+++ inc/filtro-edif-guardias.php
@@ -4,6 +4,7 @@
       <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=Edificio&Numero=1'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 1</button></a>
       <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=Edificio&Numero=2'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 2</button></a>
       <a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=Edificio&Numero=3'; ?>"><button type="button" class="btn btn-success"><span></span> Edificio 3</button></a>
+      <a href="<?php echo $_SERVER['PHP_SELF'] . ''; ?>"><button type="button" class="btn btn-success"><span></span> Todos</button></a>
     </div>
   </div>
 </div>
