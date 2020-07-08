9c378eafffe0e40d8c330e14b731fc47ba08cf65
griffo <griffo@pop-os.localdomain>
2020-07-08 11:25:42 +0200

    Nueva interfaz cambio contraseña

--- inc/new_pass.php
+++ inc/new_pass.php
@@ -1,33 +1,21 @@
 <?php
 include('valida_new_pass.php');
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>Resetear Pass</title>
-	<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-	<form class="login-form" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
-		<h2 class="form-title">Nuevo password</h2>
-		<!-- form validation messages -->
-		
-		<div class="form-group">
-			<label>Contraseña Actual</label>
-			<input type="password" name="act_pass">
-		</div>
-		<div class="form-group">
-			<label>Nueva contraseña</label>
-			<input  minlength="8" type="password" name="new_pass">
-		</div>
-		<div class="form-group">
-			<label>Confirmar nueva contraseña</label>
-			<input minlength="8" type="password" name="new_pass_c">
-		</div>
-		<div class="form-group">
-			<button type="submit" name="new_password" class="login-btn">Confirmar</button>
-		</div>
-	</form>
-</body>
-</html>
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+          <!-- Tabs Titles -->
+
+          <!-- Icon -->
+        <div class="fadeIn first">
+        	 <h1>Cambiar Contraseña</h1>
+        </div>
+
+          <!-- Login Form -->
+			<form class="login-form" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+					<input type="password" name="act_pass" class="fadeIn second" placeholder="Contraseña Actual" required>
+					<input  minlength="8" type="password" name="new_pass" class="fadeIn third" placeholder="Nueva Contraseña" required>
+					<input minlength="8" type="password" name="new_pass_c" class="fadeIn third" placeholder="Confirmar Nueva Contraseña" required>
+					<input type="submit" name="new_password" value="Confirmar" class="fadeIn fourth">
+			</form>
+    </div>
+</div>
