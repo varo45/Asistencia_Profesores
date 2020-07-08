7cb34916812ed885bb9ea24697bbad0f8b5dd44c
varo <varo@pop-os.localdomain>
2020-07-08 10:36:41 +0200

    login css

--- public/js/profesores.js
+++ public/js/profesores.js
@@ -2,6 +2,7 @@
     $('.row_show').on('click', function() {
         id = $(this).attr('id').split('_'),
         id = id[1],
-        $("#horario").load('index.php?ACTION=profesores&profesor=' + id)
+        $("#horario").load('index.php?ACTION=profesores&profesor=' + id),
+        $("html, body").animate({ scrollTop: 0 })
     });
 </script>
