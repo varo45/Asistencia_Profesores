03393d2443eead841080bedddfa5e9797c622acf
varo <varo@pop-os.localdomain>
2020-08-12 11:37:32 +0200

    refactor de menús

--- inc/top-nav.php
+++ inc/top-nav.php
@@ -21,88 +21,141 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-menu">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="<?php echo "index.php"; ?>">Netasys</a>
-    </div>
-    <div class="collapse navbar-collapse" id="top-menu">
-    <ul class="nav navbar-nav">
-      <li class="<?php echo $act_home; ?>"><a href="<?php echo "index.php"; ?>"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
-      <li class="dropdown <?php echo $act_horario; ?>"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-calendar"></span> Horario <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=horarios' ?>"><span class="glyphicon glyphicon-calendar"></span> Consultar horario</a></li>
-      <?php
-        if($_SESSION['Perfil'] === 'Admin')
-        {
-      ?>
-          <li><a href="index.php?ACTION=import-horario"><span class="glyphicon glyphicon-open"></span> Importar horarios</a></li>
-      <?php
-        }
-      ?>
-        </ul>
-      </li>
-      <?php
-        if($_SESSION['Perfil'] === 'Profesor')
-        {
-      ?>
-          <li class="<?php echo $act_asistencia; ?>"><a href="<?php $d = date('d'); $m = date('m'); $Y = date('Y'); echo $_SERVER['PHP_SELF'] . "?ACTION=asistencias&d=$d&m=$m&Y=$Y"; ?>"><span class="glyphicon glyphicon-check"></span> Mis asistencias</a></li>
-      <?php
-        }
-      ?>
-      <?php
-        if($_SESSION['Perfil'] === 'Admin')
-        {
-            echo "<li class='dropdown $act_profesores '><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-education'></span> Profesores <span class='caret'></a>";
+<?php
+
+if($response = $class->selectFrom("SELECT count(*) as new FROM Mensajes WHERE (ID_PROFESOR='$_SESSION[ID]' AND Borrado_Profesor=0) OR (ID_DESTINATARIO='$_SESSION[ID]' AND Borrado_Destinatario=0) ORDER BY ID DESC"))
+{
+  $new = $response->fetch_assoc();
+  if($new['new'] > 0)
+  {
+    $notificacion = " <span class='badge'>$new[new]</span>";
+  }
+  else
+  {
+    $notificacion = '';
+  }
+}
+else
+{
+  $ERR_MSG = $class->ERR_NETASYS;
+}
+echo '<nav class="navbar navbar-inverse navbar-fixed-top">';
+  echo '<div class="container-fluid">';
+    echo '<div class="navbar-header">';
+      echo '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#top-menu">';
+        echo '<span class="icon-bar"></span>';
+        echo '<span class="icon-bar"></span>';
+        echo '<span class="icon-bar"></span>';
+      echo '</button>';
+      echo '<a class="navbar-brand" href="index.php">Netasys</a>';
+    echo '</div>';
+    echo '<div class="collapse navbar-collapse" id="top-menu">';
+
+      if($_SESSION['Perfil'] === 'Admin')
+      {
+        echo '<ul class="nav navbar-nav">';
+          echo "<li class='$act_home'>";
+            echo "<a href='index.php'><span class='glyphicon glyphicon-home'></span> Inicio</a>";
+          echo "</li>";
+
+          echo "<li class='dropdown $act_horario'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-calendar'></span> Horario <span class='caret'></span></a>";
+            echo '<ul class="dropdown-menu">';
+              echo "<li><a href='$_SERVER[PHP_SELF]?ACTION=horarios'><span class='glyphicon glyphicon-calendar'></span> Consultar horario</a></li>";
+              echo '<li><a href="index.php?ACTION=import-horario"><span class="glyphicon glyphicon-open"></span> Importar horarios</a></li>';
+            echo '</ul>';
+          echo '</li>';
+
+          echo "<li class='dropdown $act_profesores '><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-education'></span> Profesores <span class='caret'></a>";
             echo "<ul class='dropdown-menu'>";
               echo "<li><a href='$_SERVER[PHP_SELF]?ACTION=profesores'><span class='glyphicon glyphicon-education'></span> Mostrar profesores</a></li>";
               echo "<li><a href='$_SERVER[PHP_SELF]?ACTION=import-profesorado'><span class='glyphicon glyphicon-plus'></span> Importar profesores</a></li>";
             echo "</ul>";
-            echo "</li>";
+          echo "</li>";
 
-            echo "<li><a href='$_SERVER[PHP_SELF]?ACTION=lectivos'><span class='glyphicon glyphicon-calendar'></span> Calendario escolar</a></li>";
-        }
-    ?>
-      <!--li class="<?php echo $act_guardias; ?>"><a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=guardias' ?>"><span class="glyphicon glyphicon-eye-open"></span> Guardias</a></li-->
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown <?php echo $act_usuario; ?>"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span> 
-      <?php 
+          echo "<li>";
+            echo "<a href='$_SERVER[PHP_SELF]?ACTION=lectivos'><span class='glyphicon glyphicon-calendar'></span> Calendario escolar</a>";
+          echo "</li>";
+        echo "</ul>";
 
-        echo $_SESSION['Nombre'];
-        
-        if($response = $class->selectFrom("SELECT count(*) as new FROM Mensajes WHERE (ID_PROFESOR='$_SESSION[ID]' AND Borrado_Profesor=0) OR (ID_DESTINATARIO='$_SESSION[ID]' AND Borrado_Destinatario=0) ORDER BY ID DESC"))
-        {
-          $new = $response->fetch_assoc();
-          if($new['new'] > 0)
-          {
-            echo " <span class='badge'>$new[new]</span>";
-          }
-        }
-        echo '<span class="caret"></span></a>';
-        echo '<ul class="dropdown-menu">';
-        if($_SESSION['Perfil'] === 'Profesor')
-        {
-          echo '<li><a href="index.php?ACTION=qrcoder"><span class="glyphicon glyphicon-qrcode"></span> Mi código QR</a></li>';
-        }
-        echo '<li><a href="index.php?ACTION=form_mensajes"><span class="glyphicon glyphicon-comment"></span> Mensajes ';
-        if($new['new'] > 0)
-        {
-          echo " <span class='badge'>$new[new]</span>";
-        }
-        echo '</a>';
-        echo '</li>';
-        ?>
-        <li><a href="index.php?ACTION=cambio_pass"><span id="cambio-pass" class="glyphicon glyphicon-refresh"></span> Cambio de contraseña</a></li>    
-        </ul>
-      </li>
-      <li><a href="<?php echo $_SERVER['PHP_SELF'] . '?ACTION=logout' ?>"><span class="glyphicon glyphicon-log-out"></span> Cerrar Sesión</a></li>
-    </ul>
-  </div>
-  </div>
-</nav>
+        echo '<ul class="nav navbar-nav navbar-right">';
+          echo "<li class='dropdown $act_usuario'>";
+            echo "<a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-user'></span> ";
+              echo $_SESSION['Nombre'];
+              echo $notificacion;
+              echo '<span class="caret"></span>';
+            echo '</a>';
+            echo '<ul class="dropdown-menu">';
+              echo '<li>';
+                echo'<a href="index.php?ACTION=form_mensajes"><span class="glyphicon glyphicon-comment"></span> Mensajes ';
+                  echo $notificacion;
+                echo '</a>';
+              echo '</li>';
+              echo '<li>';
+                echo '<a href="index.php?ACTION=admon"><span class="glyphicon glyphicon-folder-open"></span> Administración</a>';
+              echo '</li>';
+              echo '<li>';
+                echo '<a href="index.php?ACTION=cambio_pass"><span id="cambio-pass" class="glyphicon glyphicon-refresh"></span> Cambio de contraseña</a>';
+              echo '</li>';    
+            echo '</ul>';
+          echo '</li>';
+
+          echo "<li>";
+            echo "<a href='$_SERVER[PHP_SELF]?ACTION=logout'><span class='glyphicon glyphicon-log-out'></span> Cerrar Sesión</a>";
+          echo "</li>";
+        echo '</ul>';
+      }
+      
+      if($_SESSION['Perfil'] === 'Profesor')
+      {
+        $d = date('d');
+        $m = date('m');
+        $Y = date('Y');
+        echo '<ul class="nav navbar-nav">';
+          echo "<li class='$act_home'>";
+            echo "<a href='index.php'><span class='glyphicon glyphicon-home'></span> Inicio</a>";
+          echo "</li>";
+
+          echo "<li class='dropdown $act_horario'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-calendar'></span> Horario <span class='caret'></span></a>";
+            echo '<ul class="dropdown-menu">';
+              echo "<li>";
+                echo "<a href='$_SERVER[PHP_SELF]?ACTION=horarios'><span class='glyphicon glyphicon-calendar'></span> Consultar horario</a>";
+              echo "</li>";
+            echo '</ul>';
+          echo '</li>';
+          
+          echo "<li class='$act_asistencia'>";
+            echo "<a href='$_SERVER[PHP_SELF]?ACTION=asistencias&d=$d&m=$m&Y=$Y'><span class='glyphicon glyphicon-check'></span> Mis asistencias</a>";
+          echo "</li>";
+        echo '</ul>';
+
+        echo '<ul class="nav navbar-nav navbar-right">';
+          echo "<li class='dropdown $act_usuario'>";
+            echo "<a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-user'></span> ";
+              echo $_SESSION['Nombre'];
+              echo $notificacion;
+              echo '<span class="caret"></span>';
+            echo '</a>';
+            echo '<ul class="dropdown-menu">';
+              echo '<li>';
+                echo '<a href="index.php?ACTION=qrcoder"><span class="glyphicon glyphicon-qrcode"></span> Mi código QR</a>';
+              echo '</li>';
+              echo '<li>';
+                echo'<a href="index.php?ACTION=form_mensajes"><span class="glyphicon glyphicon-comment"></span> Mensajes ';
+                  echo $notificacion;
+                echo '</a>';
+              echo '</li>';
+              echo '<li>';
+                echo '<a href="index.php?ACTION=cambio_pass"><span id="cambio-pass" class="glyphicon glyphicon-refresh"></span> Cambio de contraseña</a>';
+              echo '</li>';    
+            echo '</ul>';
+          echo '</li>';
+
+          echo "<li>";
+            echo "<a href='$_SERVER[PHP_SELF]?ACTION=logout'><span class='glyphicon glyphicon-log-out'></span> Cerrar Sesión</a>";
+          echo "</li>";
+        echo '</ul>';
+      }
+
+    echo '</div>';
+  echo '</div>';
+echo '</nav>';
