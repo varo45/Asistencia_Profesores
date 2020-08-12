51453263f8703deb9f90fac4d907194bc3aed154
varo <varo@pop-os.localdomain>
2020-08-12 13:49:01 +0200

    menu con export

--- inc/menu_admon.php
+++ inc/menu_admon.php
@@ -1,9 +1,11 @@
 <div class="container" id="botonera" style="margin-top:75px">
     <div class="row"> 
         <div class="col-xs-12">
-            <a enlace="index.php?ACTION=admon_select&select=asistencias" class="btn btn-info btn-select">Listados Asistencias</a>
-            <a enlace="index.php?ACTION=admon_select&select=faltas" class="btn btn-info btn-select">Listados Faltas</a>
-            <a enlace="index.php?ACTION=admon_select&select=horarios" class="btn btn-info btn-select">Listados Horarios</a>
+            <a enlace="index.php?ACTION=admon_select&select=asistencias" class="btn btn-info btn-select"><span class="glyphicon glyphicon-open"></span> Exportar listados Asistencias</a>
+            <a enlace="index.php?ACTION=admon_select&select=faltas" class="btn btn-info btn-select"><span class="glyphicon glyphicon-open"></span> Exportar listados Faltas</a>
+            <a enlace="index.php?ACTION=admon_select&select=horarios" class="btn btn-info btn-select"><span class="glyphicon glyphicon-open"></span> Exportar listados Horarios</a>
+            </br></br>
+            <a enlace="index.php?ACTION=admon_select&select=horarios" class="btn btn-success btn-select"><span class="glyphicon glyphicon-eye-open"></span> Mostrar listados Horarios</a>
         </div>
         </br>
         <div class="col-xs-12">
