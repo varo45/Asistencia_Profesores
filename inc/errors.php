6ad5fed47bec3c8c3cd3511bb71eb9cf53ef411a
varo <varo@pop-os.localdomain>
2020-08-14 12:24:51 +0200

    Errors GET method

--- inc/errors.php
+++ inc/errors.php
@@ -57,6 +57,35 @@ elseif(isset($class->ERR_NETASYS))
   </div>
 ';
 }
+elseif(isset($_GET['ERR_MSG']))
+{
+  echo "
+  <script>
+  window.onload = function() {
+    $('#ERR_MSG_MODAL').modal('show')
+  };
+  </script>
+  ";
+  echo '
+  <!-- Modal -->
+  <div class="modal fade" id="ERR_MSG_MODAL" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p style="color: red;">
+            ' . $_GET['ERR_MSG'] . '
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+';
+}
 elseif(isset($MSG))
 {
   echo "
