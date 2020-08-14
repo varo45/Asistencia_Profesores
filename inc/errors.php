f889762143a82f09bbe404099ae192224a28f42e
varo <varo@pop-os.localdomain>
2020-08-14 12:28:45 +0200

    $_GET vars

--- inc/errors.php
+++ inc/errors.php
@@ -86,6 +86,35 @@ elseif(isset($_GET['ERR_MSG']))
   </div>
 ';
 }
+elseif(isset($_GET['MSG']))
+{
+  echo "
+  <script>
+  window.onload = function() {
+    $('#MSG_MODAL').modal('show')
+  };
+  </script>
+  ";
+  echo '
+  <!-- Modal -->
+  <div class="modal fade" id="MSG_MODAL" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p style="color: green;">
+            ' . $_GET['MSG'] . '
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+';
+}
 elseif(isset($MSG))
 {
   echo "
