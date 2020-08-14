49ceaa2daedb5a4c59d59b7112bddb43c0d16ff8
varo <varo@pop-os.localdomain>
2020-08-14 14:52:57 +0200

    index style sustituto

--- public/index.php
+++ public/index.php
@@ -680,6 +680,28 @@ if(isset($_GET['ACTION']))
           {
             if($class->compruebaCambioPass())
             {
+              $style = '
+                #select_sustituto {
+                  background-color: #f6f6f6;
+                  border: none;
+                  color: #0d0d0d;
+                  padding: 15px 32px;
+                  text-align: center;
+                  text-decoration: none;
+                  display: inline-block;
+                  font-size: 16px;
+                  margin: 5px;
+                  width: 85%;
+                  border: 2px solid #f6f6f6;
+                  -webkit-transition: all 0.5s ease-in-out;
+                  -moz-transition: all 0.5s ease-in-out;
+                  -ms-transition: all 0.5s ease-in-out;
+                  -o-transition: all 0.5s ease-in-out;
+                  transition: all 0.5s ease-in-out;
+                  -webkit-border-radius: 5px 5px 5px 5px;
+                  border-radius: 5px 5px 5px 5px;
+                }
+              ';
               include_once($dirs['inc'] . 'top-nav.php');
               include_once($dirs['inc'] . 'formulario-sustituto.php');
               include_once($dirs['inc'] . 'errors.php');
