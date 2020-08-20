f672797a86e044e92da679c1995da27461ee1d5e
varo <varo@pop-os.localdomain>
2020-08-20 15:14:31 +0200

    Códigos QR encriptados y QR generator Google (AntiFallos)

--- inc/generate_code.php
+++ inc/generate_code.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+include_once($dirs['inc'] . 'mcript.php');
 if(isset($_SESSION['ID']) && ! empty($_SESSION['ID']))
 {
     if($response = $class->query("SELECT Iniciales, Password FROM $class->profesores WHERE ID='$_SESSION[ID]'"))
@@ -17,12 +19,20 @@ if(isset($_SESSION['ID']) && ! empty($_SESSION['ID']))
         $codeFile = uniqid().'.png';
         echo '
         <div class="container" style="margin-top:50px">
-            <div class="row">
+            <div class="row" style="text-align: center;">
                 <div class="col-xs-12">
-                <h3>Código de identificación</h3>
+                <h3>Código de fichaje</h3>
         ';
-        QRcode::png($datos['Iniciales'].';'.$datos['Password'], $codesDir.$codeFile, 'H', '10'); 
-        echo '<img class="img-thumbnail" src="'.$codesDir.$codeFile.'" />';
+                    $dato = $datos['Iniciales'] . ';' . $datos['Password'];
+                    $dato_encriptado = $encriptar($dato);
+                    QRcode::png($dato_encriptado, $codesDir.$codeFile, 'H', '10'); 
+                    echo '<img class="img-thumbnail" src="'.$codesDir.$codeFile.'" />';
+                    
+                    // Imagen QR generada por Google justo debajo 
+                    //          UTILIZAR EN CASO DE FALLO DE MÓDULO phpqrcode/qrlib.php
+                    //echo '<img class="img-thumbnail" src="https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=' . urlencode($dato_encriptado) . '&choe=UTF-8" title="Código QR" />';
+                    echo "<br><br><span>* Acerque el código al lector QR para fichar.</span>";
+
                 echo "</div>";
             echo "</div>";
         echo "</div>";
@@ -35,5 +45,3 @@ if(isset($_SESSION['ID']) && ! empty($_SESSION['ID']))
 else {
     echo $class->ERR_NETASYS;
 }
-
-?>
---
+++ inc/mcript.php
@@ -0,0 +1,34 @@
+<?php
+
+//Configuración del algoritmo de encriptación
+
+//Debes cambiar esta cadena, debe ser larga y unica
+//nadie mas debe conocerla
+$clave  = "Netasys_QR-generator?2020&C4R10S&P3DR0&4IvaR4S";
+
+//Metodo de encriptación
+$method = 'aes-256-cbc';
+/*
+Genera un valor para IV
+*/
+$getIV = function () use ($method) {
+    return base64_decode(base64_encode(openssl_cipher_iv_length($method)));
+};
+$iv = date('YYmmdd');
+
+/*
+Encripta el contenido de la variable, enviada como parametro.
+*/
+$encriptar = function ($valor) use ($method, $clave, $iv) {
+    return openssl_encrypt ($valor, $method, $clave, false, $iv);
+};
+
+/*
+Desencripta el texto recibido
+*/
+$desencriptar = function ($valor) use ($method, $clave, $iv) {
+    $encrypted_data = base64_decode($valor);
+    return openssl_decrypt($valor, $method, $clave, false, $iv);
+};
+ 
+
