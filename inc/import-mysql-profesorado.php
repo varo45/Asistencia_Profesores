6e2da6bd8dd0f56f545390e7119e3408e63451eb
griffo <griffo@pop-os.localdomain>
2020-08-25 13:32:51 +0200

    Comprobar columnas csv

--- inc/import-mysql-profesorado.php
+++ inc/import-mysql-profesorado.php
@@ -15,41 +15,50 @@ if (isset($_POST["import"])) {
         $file = fopen($fileName, "r");
         $row = 1;
         while (($column = fgetcsv($file, 10000, ";")) !== FALSE) {
-            if($row == 1){ $row++; continue; }
-            $row++;
-            $iniciales = "";
-            if (isset($column[0])) {
-                $iniciales = mysqli_real_escape_string($conn, utf8_encode($column[0]));
-            }
-            $nombre = "";
-            if (isset($column[1])) {
-                $nombre = mysqli_real_escape_string($conn, utf8_encode($column[1]));
-            }
-            $tutor = "";
-            if (isset($column[2])) {
-                $tutor = mysqli_real_escape_string($conn, utf8_encode($column[2]));
-            }
-            $password = $class->encryptPassword($iniciales . '12345');
-            $tipo = mysqli_real_escape_string($conn, utf8_encode(2));
-            $activo = mysqli_real_escape_string($conn, utf8_encode(1));
-            $sustituido = mysqli_real_escape_string($conn, utf8_encode(0));
-            if(! $class->query("INSERT INTO Profesores (Iniciales, Nombre, Password, TIPO, Tutor, Activo, Sustituido)
-            values (
-                '$iniciales',
-                '$nombre',
-                '$password',
-                '$tipo',
-                '$tutor',
-                $activo,
-                $sustituido)"))
+            $columnas = count($column);
+            if($columnas == 3)
             {
-                $ERR_MSG = "<br>Error al importar datos desde CSV.<br>";
-                $ERR_MSG .= $class->ERR_NETASYS;
+                if($row == 1){ $row++; continue; }
+                $row++;
+                $iniciales = "";
+                if (isset($column[0])) {
+                    $iniciales = mysqli_real_escape_string($conn, utf8_encode($column[0]));
+                }
+                $nombre = "";
+                if (isset($column[1])) {
+                    $nombre = mysqli_real_escape_string($conn, utf8_encode($column[1]));
+                }
+                $tutor = "";
+                if (isset($column[2])) {
+                    $tutor = mysqli_real_escape_string($conn, utf8_encode($column[2]));
+                }
+                $password = $class->encryptPassword($iniciales . '12345');
+                $tipo = mysqli_real_escape_string($conn, utf8_encode(2));
+                $activo = mysqli_real_escape_string($conn, utf8_encode(1));
+                $sustituido = mysqli_real_escape_string($conn, utf8_encode(0));
+                if(! $class->query("INSERT INTO Profesores (Iniciales, Nombre, Password, TIPO, Tutor, Activo, Sustituido)
+                values (
+                    '$iniciales',
+                    '$nombre',
+                    '$password',
+                    '$tipo',
+                    '$tutor',
+                    $activo,
+                    $sustituido)"))
+                {
+                    $ERR_MSG = "<br>Error al importar datos desde CSV.<br>";
+                    $ERR_MSG .= $class->ERR_NETASYS;
+                }
+                else
+                {
+                    $MSG = "Profesores importados correctamente.<br>";
+                }
             }
             else
             {
-                $MSG = "Profesores importados correctamente.<br>";
+                $ERR_MSG = "<br>Error en Fichero, no es el esperado.<br>";
             }
+            
         }
     }
 }
